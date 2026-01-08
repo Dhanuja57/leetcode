5aed2ebc9c885d148f5d3d6ddcefa8ba14272b0c
Dhanuja57 <shridhanuja@gmail.com>
2026-01-08 11:31:48 +0530

    Time: 3 ms (87.21%) | Memory: 52.3 MB (57.92%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,52 +1,31 @@
 class Solution {
-    int[] parent;
-    int count;
-
-    public int numIslands(char[][] grid) {
-        int r = grid.length, c = grid[0].length;
-        parent = new int[r * c];
-        count = 0;
-
-        // initialize
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == '1') {
-                    int id = i * c + j;
-                    parent[id] = id;
-                    count++; // each '1' is initially an island
-                }
-            }
+    static int r;
+    static int c;
+    public void dfs(char grid[][],int i,int j,boolean visited[][]){
+        if(i<0|| i>=r||j<0||j>=c || visited[i][j]==true){
+            return;
         }
-
-        int[][] dirs = {{1,0},{0,1}};
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == '1') {
-                    for (int[] d : dirs) {
-                        int ni = i + d[0], nj = j + d[1];
-                        if (ni < r && nj < c && grid[ni][nj] == '1') {
-                            union(i * c + j, ni * c + nj);
-                        }
-                    }
+       if(grid[i][j]=='1' ){
+         visited[i][j]=true;
+       dfs(grid,i-1,j,visited);
+       dfs(grid,i+1,j,visited);
+       dfs(grid,i,j-1,visited);
+       dfs(grid,i,j+1,visited);
+       }
+    }
+    public int numIslands(char[][] grid) {
+         r=grid.length;
+         c=grid[0].length;
+        boolean[][] visited=new boolean[r][c];
+        int count=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]=='1' && visited[i][j]==false){
+                    count++;
+                    dfs(grid,i,j,visited);
                 }
-            }
+            }  
         }
         return count;
-    }
-
-    int find(int x) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x]); // path compression
-        return parent[x];
-    }
-
-    void union(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        if (px != py) {
-            parent[py] = px;
-            count--; // two islands merged into one
-        }
     }
 }
