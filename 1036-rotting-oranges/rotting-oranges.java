45a8717613d77f3c6872345c9b7b44640949d8e7
Dhanuja57 <shridhanuja@gmail.com>
2026-01-11 18:56:50 +0530

    Time: 2 ms (90.28%) | Memory: 44.1 MB (60.19%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,37 +1,57 @@
 class Solution {
-    private void dfs(int[][] grid,int[][] time,int i,int j,int ct){
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length||grid[i][j]==0 || ct>=time[i][j]) return;
-
-        time[i][j]=ct;
-        dfs(grid,time,i-1,j,ct+1);
-        dfs(grid,time,i+1,j,ct+1);
-        dfs(grid,time,i,j-1,ct+1);
-        dfs(grid,time,i,j+1,ct+1);
-
-    }
-    public int orangesRotting(int[][] grid) {
-        if(grid==null || grid.length==0) return -1;
-        int r=grid.length,c=grid[0].length;
-        int [][] time=new int[r][c];
-        for(int i=0;i<r;i++){
-            Arrays.fill(time[i],Integer.MAX_VALUE);
-        }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-              if(grid[i][j]==2){
-                dfs(grid,time,i,j,0);
-              }
+    static int row;
+    static int col;
+    static int bfs(int[][] grid, Queue<int[]> queue){
+        int[] arr = new int[3];
+        while(!queue.isEmpty()){
+           
+            arr = queue.poll();
+            
+            
+            if(arr[0] -1 >= 0  &&  grid[arr[0]-1][arr[1]]==1 ){
+                    grid[arr[0]-1][arr[1]]=2;
+                    queue.add(new int[] {arr[0]-1,arr[1],arr[2]+1});
             }
+            if(arr[0]+1 < row &&  grid[arr[0]+1][arr[1]]==1 ){
+                    grid[arr[0]+1][arr[1]]=2;
+                    queue.add(new int[] {arr[0]+1,arr[1],arr[2]+1});
+            }
+            if(arr[1] -1 >= 0 &&  grid[arr[0]][arr[1]-1]==1 ){
+                grid[arr[0]][arr[1]-1]=2;
+                queue.add(new int[] {arr[0],arr[1]-1,arr[2]+1});
+            }
+            if(arr[1]+1 < col &&  grid[arr[0]][arr[1]+1]==1 ){
+                    grid[arr[0]][arr[1]+1]=2;
+                    queue.add(new int[] {arr[0],arr[1]+1,arr[2]+1});
+            }
+            
         }
-        int tr=0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(grid[i][j]==1){
-                    if(time[i][j]==Integer.MAX_VALUE) return -1;
-                    tr=Math.max(tr,time[i][j]);
+        return arr[2];
+    }
+
+    public int orangesRotting(int[][] grid) {
+        row = grid.length;
+        col = grid[0].length;
+        Queue<int[] > queue = new LinkedList<>();
+        for(int i=0; i<row; i++ ){
+            for(int j =0; j< col; j++){
+                if(grid[i][j]==2){
+                    int[] arr = {i,j,0}; 
+                    queue.add(arr);
+                  
                 }
             }
         }
-        return tr;
+        int max = bfs(grid,queue);
+
+        for(int i=0; i<row; i++ ){
+            for(int j =0; j< col; j++){
+                if(grid[i][j]==1){
+                    return -1;
+                   
+                }
+            }
+        }         
+        return max;
     }
 }
