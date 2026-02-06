f2318d970e0839c7405385e4e4b860606aa6b8f7
Dhanuja57 <shridhanuja@gmail.com>
2026-02-07 00:18:14 +0530

    Time: 419 ms (5.40%) | Memory: 30.4 MB (36.58%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.c
@@ -0,0 +1,23 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    int *ans=(int*) malloc(numsSize*sizeof(int));
+    int prefix[numsSize];
+    int surfix[numsSize];
+    prefix[0]=1;
+    surfix[numsSize-1]=1;
+    for(int i=1;i<numsSize;i++){
+        printf("hii prefix");
+        prefix[i]=prefix[i-1]*nums[i-1];
+    }
+    for(int j=numsSize-2;j>=0;j--){
+        printf("hi sur");
+        surfix[j]=surfix[j+1]*nums[j+1];
+    }
+    for(int itr=0;itr<numsSize;itr++){
+        ans[itr]=prefix[itr]*surfix[itr];
+    }
+    *returnSize = numsSize;
+    return ans;
+}
