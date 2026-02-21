271870a8c495258f98f40c57515516bcd96eec56
Dhanuja57 <shridhanuja@gmail.com>
2026-02-21 10:15:42 +0530

    Time: 4 ms (98.60%) | Memory: 47.3 MB (91.66%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,40 +1,31 @@
 class Solution {
     int dirs[][] = {{-1,0},{1,0},{0,1},{0,-1}};
-
-    void bfs(int X, int Y, boolean vis[][], int heights[][]) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{X, Y});
-        vis[X][Y] = true;
-
-        int n = heights.length, m = heights[0].length;
-        while (!q.isEmpty()) {
-            int[] top = q.remove();
-            int x = top[0], y = top[1];
-
-            for (int[] d : dirs) {
-                int nx = x + d[0], ny = y + d[1];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m &&
-                    !vis[nx][ny] && heights[nx][ny] >= heights[x][y]) {
-                    vis[nx][ny] = true;
-                    q.add(new int[]{nx, ny});
-                }
+     int m,n=0;
+    void dfs(int x, int y, boolean vis[][], int heights[][]) {
+        vis[x][y]=true;
+        for(int[] dir : dirs){
+            int ni=x+dir[0];
+            int nj=y+dir[1];
+            if(ni>=0 && ni<n && nj >=0 && nj<m && !vis[ni][nj] && heights[ni][nj]>=heights[x][y] ){
+                dfs(ni,nj,vis,heights);
             }
         }
     }
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> list = new ArrayList<>();
-        int n = heights.length, m = heights[0].length;
+         n = heights.length;
+          m = heights[0].length;
         boolean po[][] = new boolean[n][m];
         boolean ao[][] = new boolean[n][m];
 
         for (int i = 0; i < n; i++) {
-            bfs(i, 0, po, heights);
-            bfs(i, m - 1, ao, heights);
+            dfs(i, 0, po, heights);
+            dfs(i, m - 1, ao, heights);
         }
         for (int j = 0; j < m; j++) {
-            bfs(0, j, po, heights);
-            bfs(n - 1, j, ao, heights);
+            dfs(0, j, po, heights);
+            dfs(n - 1, j, ao, heights);
         }
 
         for (int i = 0; i < n; i++) {
